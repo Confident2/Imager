0839606287816292647c70aa8d7882167306ef75
confident2 <mykel.anigboya@yahoo.com>
2023-05-08 16:09:34 +0100

    second commitment

--- src/component/ImageDisplay.jsx
+++ src/component/ImageDisplay.jsx
@@ -1,18 +1,17 @@
 // components/ImageDisplay.js
 import React from 'react';
 import imageData from '../data/imageData.json';
-import { FaHeart, FaPlus } from 'react-icons/fa'; // Import the love and plus icons from react-icons library
+import { FaHeart, FaPlus } from 'react-icons/fa';
+import Image from 'next/image'; // Import the Image component from Next.js
 
 const ImageDisplay = () => {
   return (
-    <div className="w-full max-w-7xl columns-4  gap-3px gap-5  pt-2 pb-8 mx-auto mb-10 ">
+    <div className="w-full max-w-7xl columns-4 gap-3px gap-5 pt-2 pb-8 mx-auto mb-10">
       {imageData.images.map(({ id, description, author, url, tags }) => (
         <div key={id} className="relative">
-          <img src={url} alt={description} className="w-full h-full object-cover mb-3" />
+          <Image src={url} alt={description} width={400} height={400} objectFit="cover" className="mb-3" />
           <div className="absolute inset-0 bg-black opacity-0 transition-opacity duration-300 hover:opacity-75 flex items-center justify-center">
-            {/* Love icon */}
             <FaHeart className="text-white text-3xl mr-3 hover:text-red-500 transition-colors duration-300" />
-            {/* Plus icon */}
             <FaPlus className="text-white text-3xl hover:text-blue-500 transition-colors duration-300" />
           </div>
         </div>
--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,65 +1,65 @@
-import { Header } from '@/component/header';
-import React, { Component } from 'react'
+import { Header } from "@/component/header";
+import React, { Component } from "react";
 
-
- class Signup extends Component {
-  state = { password: '' }
+class Signup extends Component {
+  state = { password: "" };
   render() {
-
     return (
       <div>
         <Header />
-      <div
-        className=" h-96 bg-cover bg-center flex justify-center items-center saturate-150 contrast-250   bg-[url('/images/confiHeader.jpg')] " style={{'height':'40rem'}} >
-      
-
-        <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col w-1/2">
-          <h1 className="text-2xl font-bold mb-4 text-center text-gray-800">
-            Sign Up
-          </h1>
-          <label htmlFor="name" className="sr-only">
-            Name
-          </label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            placeholder="Name"
-          />
-          <label htmlFor="email" className="sr-only">
-            Email
-          </label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mt-4"
-            placeholder="Email"
-          />
-          <label htmlFor="password" className="sr-only">
-            Password
-          </label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mt-4"
-            placeholder="Password"
-            value={this.state.password}
-            onChange={(e) => this.setState({ password: e.target.value })}
-          />
-          {this.state.password.length < 4 ? 'Password must be at least 4 characters' : ''}
-          <button
-            type="submit"
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mt-4"
-          >
-            Sign Up
-          </button>
+        <div
+          className=" h-96 bg-cover bg-center flex justify-center items-center saturate-150 contrast-250   bg-[url('/images/confiHeader.jpg')] "
+          style={{ height: "40rem" }}
+        >
+          <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col w-1/2">
+            <h1 className="text-2xl font-bold mb-4 text-center text-gray-800">
+              Sign Up
+            </h1>
+            <label htmlFor="name" className="sr-only">
+              Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              placeholder="Name"
+            />
+            <label htmlFor="email" className="sr-only">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mt-4"
+              placeholder="Email"
+            />
+            <label htmlFor="password" className="sr-only">
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mt-4"
+              placeholder="Password"
+              value={this.state.password}
+              onChange={(e) => this.setState({ password: e.target.value })}
+            />
+            {this.state.password.length < 4
+              ? "Password must be at least 4 characters"
+              : ""}
+            <button
+              type="submit"
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mt-4"
+            >
+              Sign Up
+            </button>
+          </div>
         </div>
       </div>
-      </div>
-    )
+    );
   }
 }
 
